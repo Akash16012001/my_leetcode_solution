ab3a1f14783f92f7d5e879bc7d2cecbc7b5b6ddc
Akash <107176538+Akash16012001@users.noreply.github.com>
2023-09-12 17:47:17 +0530

    Time: 65 ms (73.06%), Space: 17.9 MB (8.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numMap = {}
-        n = len(nums)
+        # n = len(nums)
 
         for i, j in enumerate(nums):
             component = target - j
-            if component in numMap:
-                return [numMap[component], i]
-            numMap[j] = i
+            if j in numMap:
+                return [numMap[j], i]
+            numMap[component] = i
 
 #   2,7,11,15

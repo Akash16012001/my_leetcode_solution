947b58f0af741c18281e8a08d3c470dbbc4c8cc4
Akash <107176538+Akash16012001@users.noreply.github.com>
2023-09-29 10:21:57 +0530

    Time: 2 ms (62.86%), Space: 56.1 MB (25.58%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        if(nums.length < 2) return true;
-
-        int direction = 0;
+        boolean increasing = true;
+        boolean decreasing = true;
 
         for(int i = 1; i < nums.length; i++){
-            if(nums[i - 1] < nums[i]){
-                if(direction == 0) direction = 1;
-                else if(direction == -1) return false;
+            if(nums[i] > nums[i - 1]){
+                decreasing = false;
             }
             else if(nums[i] < nums[i - 1]){
-                if(direction == 0) direction = -1;
-                else if(direction == 1) return false;
+                increasing = false;
+            }
+            if(!increasing && !decreasing){
+                return false;
             }
         }
         return true;

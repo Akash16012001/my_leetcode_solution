d2b89447b7b3a8bd6942d27f335ef36754ebf2de
Akash <107176538+Akash16012001@users.noreply.github.com>
2023-09-30 08:37:17 +0530

    Time: 25 ms (45.99%), Space: 62.9 MB (78.26%) - LeetHub

---
+++ 0456-132-pattern/0456-132-pattern.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        int third = Integer.MIN_VALUE;
+
+        for(int i = nums.length - 1; i >= 0; i--){
+            if(nums[i] < third) return true;
+            while(!st.isEmpty() && st.peek() < nums[i]){
+                third = st.pop();
+            }
+            st.push(nums[i]);
+        }
+        return false;
+    }
+}
+
+//  [3,1,4,2]
+//  [-1,3,2,0]

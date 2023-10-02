5d8b2df2facfabb77d2c791334bd37d12e0925e3
Akash <107176538+Akash16012001@users.noreply.github.com>
2023-10-02 11:51:34 +0530

    Time: 14 ms (69.83%), Space: 44 MB (81.16%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,19 +1,26 @@
-class Solution {
+public class Solution {
     public boolean winnerOfGame(String colors) {
-        int aliceScore = 0;
-        int bobScore = 0;
-
-        for(int i = 1; i < colors.length() - 1; i++){
-            char currentColor = colors.charAt(i);
-            char prevColor = colors.charAt(i - 1);
-            char nextColor = colors.charAt(i + 1);
-            if(currentColor == 'A' && prevColor == 'A' && nextColor == 'A'){
-                aliceScore++;
-            }
-            else if(currentColor == 'B' && prevColor == 'B' && nextColor == 'B'){
-                bobScore++;
+        int alice_plays = 0, bob_plays = 0, count = 0;
+        
+        for (int i = 1; i < colors.length(); i++) {
+            if (colors.charAt(i) == colors.charAt(i - 1)) {
+                count++;
+            } else {
+                if (colors.charAt(i - 1) == 'A') {
+                    alice_plays += Math.max(0, count - 1);
+                } else {
+                    bob_plays += Math.max(0, count - 1);
+                }
+                count = 0;
             }
         }
-        return aliceScore > bobScore;
+
+        if (colors.charAt(colors.length() - 1) == 'A') {
+            alice_plays += Math.max(0, count - 1);
+        } else {
+            bob_plays += Math.max(0, count - 1);
+        }
+        
+        return alice_plays > bob_plays;
     }
 }

7d8405ae75fb1d5f07925ee81be442fde2a014ad
Akash <107176538+Akash16012001@users.noreply.github.com>
2024-06-03 15:35:28 +0530

    Time: 1 ms (99.8%), Space: 41.7 MB (83.88%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.java
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -1,11 +1,9 @@
-class Solution {
+public class Solution {
     public int scoreOfString(String s) {
         int score = 0;
-        for(int i = 0; i < s.length() - 1; i++){
-            int diff = Math.abs((int)s.charAt(i) - (int)s.charAt(i + 1));
-            score += diff;
+        for (int i = 0; i < s.length() - 1; i++) {
+            score += Math.abs(s.charAt(i) - s.charAt(i + 1));
         }
-
         return score;
     }
 }

36b3756bc9a80bcf189c64181fecc4e2543bfae1
Akash <107176538+Akash16012001@users.noreply.github.com>
2024-06-11 13:11:17 +0530

    Time: 0 ms (100%), Space: 41.8 MB (84.05%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.java
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -1,38 +1,29 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] res = new int[arr1.length];
-        int index = 0;
-
-        for(int ele : arr1){
-            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        int length = 0;
+        for(int i=0;i<arr1.length;i++){
+            length = Math.max(length,arr1[i]);
         }
-
-        for(int i = 0; i < arr2.length; i++){
-            int count = map.get(arr2[i]);
-            for (int j = 0; j < count; j++){
-                res[index++] = arr2[i];
-            }
-            map.remove(arr2[i]);
+        int[] count = new int[length+1];
+        for(int i=0;i<arr1.length;i++){
+            count[arr1[i]]++;
         }
-
-        List<Integer> remaining = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int key = entry.getKey();
-            int count = entry.getValue();
-            for (int j = 0; j < count; j++) {
-                remaining.add(key);
+        int[] ans = new int[arr1.length];
+        int index=0;
+        for(int i=0;i<arr2.length;i++){
+            while(count[arr2[i]]>0){
+                ans[index] = arr2[i];
+                index++;
+                count[arr2[i]]--;
             }
         }
-
-        // Sort the remaining elements
-        Collections.sort(remaining);
-
-        // Add sorted remaining elements to the result array
-        for (int ele : remaining) {
-            res[index++] = ele;
+        for(int i=0;i<count.length;i++){
+            while(count[i]>0){
+                ans[index] = i;
+                index++;
+                count[i]--;
+            }
         }
-
-        return res;
+        return ans;
     }
 }
